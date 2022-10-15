221c9bc8cb9a1619ff2e29ff68119e45351eb12a
ADMIN <ADMIN@Gangster>
2022-10-15 09:17:54 +0700

    Change getAll() in Dao.java()

--- Desktop/Coffee_Order_Gui/src/dao/Dao.java
+++ Desktop/Coffee_Order_Gui/src/dao/Dao.java
@@ -13,9 +13,9 @@ import java.sql.*;
  */
 public interface Dao<T> {
     T get(int id) throws SQLException;
-    ResultSet getAll() throws SQLException;
+    List<T> getAll() throws SQLException;
     int save(T t) throws SQLException;
     int insert(T t) throws SQLException;
     int update(T t) throws SQLException;
-    int delete(T t);
+    int delete(T t) throws SQLException;
 }
