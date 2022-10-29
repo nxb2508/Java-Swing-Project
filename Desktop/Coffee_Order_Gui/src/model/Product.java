2feea0a2db906ca189412dace1e4e2a2be8902a6
ADMIN <ADMIN@Gangster>
2022-10-29 11:31:20 +0700

    Commit Product

---
+++ Desktop/Coffee_Order_Gui/src/model/Product.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name, type;
+    private double price;
+
+    public Product(){
+        
+    }
+
+    public Product(String name, String type, double price){
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    
+}
